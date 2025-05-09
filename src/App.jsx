454a0212fe63207148092de64b1c576b0831c5c3
nandini024 <nandumyakala24@gmail.com>
2025-05-10 00:49:22 +0530

    asdf

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+import React, { Component } from 'react'
+import State from '../Components/State'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <State/>
+      </div>
+    )
+  }
+}
